0b4a1abc9638f7ecb08e9c2971b4dcfb6f7511c9
karannbra <karannbra@gmail.com>
2019-11-28 18:51:34 +0100

    non

--- _121/src/main/java/Main.java
+++ _121/src/main/java/Main.java
@@ -2,15 +2,13 @@ public class Main {
     public static void main(String[] args) {
 
 
-        System.out.println("cos");
-        System.out.println("cos");
-        System.out.println("cos");
-        System.out.println("cos");
-        System.out.println("cos");
-        System.out.println("cos");
-        System.out.println("cos");
-        System.out.println("cos");
-        System.out.println("cos");
+        System.out.println("asdasds");
+        System.out.println("asdasds");
+        System.out.println("asdasds");
+        System.out.println("asdasds");
+        System.out.println("asdasds");
+        System.out.println("asdasds");
+        System.out.println("asdasds");
 
 
 }}
